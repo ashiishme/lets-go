27e35bd0ea3d37d6c43d0d145e2c8b29e375cb56
ashiishme <iamashiishy@gmail.com>
2021-09-20 11:01:44 +0545

    refactor: add simple math package

---
+++ basics/packages/basicMath/main.go
@@ -0,0 +1,21 @@
+package basicMath
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func Subtract(x int, y int) int {
+	return x - y
+}
+
+func Multiply(x int, y int) int {
+	return x * y
+}
+
+func Divide(x int, y int) int {
+	return x / y
+}
+
+func Remainder(x int, y int) int {
+	return x % y
+}
