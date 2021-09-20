4bc2dabb4b7bc9b9b18bf3297c3d0ff75fb32e20
ashiishme <iamashiishy@gmail.com>
2021-09-20 11:11:48 +0545

    refactor: add SO reference for code

--- basics/packages/stringUtil/reverse.go
+++ basics/packages/stringUtil/reverse.go
@@ -1,5 +1,6 @@
 package stringUtil
 
+// REF: https://stackoverflow.com/a/10030772
 func Reverse(s string) string {
     runes := []rune(s)
     for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
