074e87a70f99c2aec28dea1994af360500e48bc3
ashiishme <iamashiishy@gmail.com>
2021-09-20 11:00:44 +0545

    refactor: add code comments

--- basics/variables/main.go
+++ basics/variables/main.go
@@ -4,11 +4,18 @@ import "fmt"
 
 func main() {
 
+	// string
 	var name string = "Ashish Yadav"
+	// integer
 	var number int = 123
+	// boolean
 	var isBoolean bool = true
 
+	// shorthand
 	shortHandString := "Software Engineer"
+
+	// multiple shorthand assginment [ This is interesting ]
+	contactNumber, address := 9877712355, "Butwal"
 
 	fmt.Println("String: ", name)
 	fmt.Println("Number: ", number)
@@ -16,4 +23,7 @@ func main() {
 	fmt.Println("Short Hand Declration: ", shortHandString)
 	// print type of the variable
 	fmt.Printf("%T\n", shortHandString)
+
+	fmt.Println("Contact: ", contactNumber)
+	fmt.Println("Address: ", address)
 }
